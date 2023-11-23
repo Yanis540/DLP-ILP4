5aab7fd5b5dd78b5e114abdd5785d94821abcf6a
Yanis540 <yanistabellout4@gmail.com>
2023-11-23 20:46:54 +0100

    [TME8] > Interpreter : done

--- Java/src/com/paracamplus/ilp4/ilp4tme8/interpreter/Interpreter.java
+++ Java/src/com/paracamplus/ilp4/ilp4tme8/interpreter/Interpreter.java
@@ -8,6 +8,7 @@ import com.paracamplus.ilp4.ilp4tme8.interfaces.IASThasProprety;
 import com.paracamplus.ilp4.ilp4tme8.interfaces.IASTreadProprety;
 import com.paracamplus.ilp4.ilp4tme8.interfaces.IASTvisitor;
 import com.paracamplus.ilp4.ilp4tme8.interfaces.IASTwriteProprety;
+import com.paracamplus.ilp4.interpreter.ILPInstance;
 import com.paracamplus.ilp4.interpreter.interfaces.IClassEnvironment;
 
 public class Interpreter  extends com.paracamplus.ilp4.interpreter.Interpreter
@@ -19,18 +20,54 @@ implements IASTvisitor<Object, ILexicalEnvironment, EvaluationException> {
     }
 
     @Override
-    public Object visit(IASTreadProprety iast, ILexicalEnvironment data) throws EvaluationException {
-        return 1; 
+    public Object visit(IASTreadProprety iast, ILexicalEnvironment lexenv) throws EvaluationException {
+        Object t = iast.getTarget().accept(this,lexenv);
+        Object p = iast.getProprety().accept(this, lexenv);
+        if (!( t instanceof ILPInstance ) ) 
+            throw new EvaluationException( "Not an ILP instances " + t);
+        if(!(p instanceof String))
+            throw new EvaluationException("Proprety should be String value but recieved " + p.getClass());
+        
+        ILPInstance target = (ILPInstance)t;
+        String proprety = (String)p; 
+
+        return target.read(proprety);
     }
 
     @Override
-    public Object visit(IASTwriteProprety iast, ILexicalEnvironment data) throws EvaluationException {
-        return 1; 
+    public Object visit(IASTwriteProprety iast, ILexicalEnvironment lexenv) throws EvaluationException {
+        Object t = iast.getTarget().accept(this,lexenv);
+        Object p = iast.getProprety().accept(this, lexenv);
+        Object value = iast.getValue().accept(this, lexenv);
+        if (!( t instanceof ILPInstance ) ) 
+            throw new EvaluationException( "Not an ILP instances " + t);
+        if(!(p instanceof String))
+            throw new EvaluationException("Proprety should be String value but recieved " + p.getClass());
+        ILPInstance target = (ILPInstance)t;
+        String proprety = (String)p; 
+        Object oldValue = target.write(proprety, value);
+        return oldValue;
     }
 
     @Override
-    public Object visit(IASThasProprety iast, ILexicalEnvironment data) throws EvaluationException {
-        return 1 ; 
+    public Object visit(IASThasProprety iast, ILexicalEnvironment lexenv) throws EvaluationException {
+        Object t = iast.getTarget().accept(this,lexenv);
+        Object p = iast.getProprety().accept(this, lexenv);
+        if (!( t instanceof ILPInstance ) ) 
+            throw new EvaluationException( "Not an ILP instances " + t);
+        if(!(p instanceof String))
+            throw new EvaluationException("Proprety should be String value but recieved " + p.getClass());
+
+        ILPInstance target = (ILPInstance)t;
+        String proprety = (String)p; 
+        try{
+            target.read(proprety);
+            return Boolean.TRUE;
+        }
+        catch(EvaluationException e){
+            return Boolean.FALSE; 
+        }
     }
+
     
 }
