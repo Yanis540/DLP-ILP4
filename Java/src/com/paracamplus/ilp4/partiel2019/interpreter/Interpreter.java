bec3c5dc2bb7bae20736582880511060d75daf30
Yanis540 <yanistabellout4@gmail.com>
2023-12-08 00:23:55 +0100

    [Partiel 2019] : working interpreter

--- Java/src/com/paracamplus/ilp4/partiel2019/interpreter/Interpreter.java
+++ Java/src/com/paracamplus/ilp4/partiel2019/interpreter/Interpreter.java
@@ -4,6 +4,7 @@ import com.paracamplus.ilp1.interpreter.interfaces.EvaluationException;
 import com.paracamplus.ilp1.interpreter.interfaces.IGlobalVariableEnvironment;
 import com.paracamplus.ilp1.interpreter.interfaces.ILexicalEnvironment;
 import com.paracamplus.ilp1.interpreter.interfaces.IOperatorEnvironment;
+import com.paracamplus.ilp4.interpreter.ILPInstance;
 import com.paracamplus.ilp4.interpreter.interfaces.IClassEnvironment;
 import com.paracamplus.ilp4.partiel2019.interfaces.IASTtryRessources;
 import com.paracamplus.ilp4.partiel2019.interfaces.IASTvisitor;
@@ -17,8 +18,22 @@ implements IASTvisitor<Object, ILexicalEnvironment, EvaluationException> {
     }
 
     @Override
-    public Object visit(IASTtryRessources iast, ILexicalEnvironment data) throws EvaluationException {
-        return 1; 
+    public Object visit(IASTtryRessources iast, ILexicalEnvironment lexenv) throws EvaluationException {
+        ILexicalEnvironment lexenv2 = lexenv; 
+        Object v = iast.getExpression().accept(this, lexenv2);
+        if(!(v instanceof ILPInstance))
+            throw new EvaluationException("Expected ILPinstance but recieved " + v.getClass().getSimpleName());
+        ILPInstance i = (ILPInstance)v; 
+        lexenv2 = lexenv2.extend(iast.getVariable(),i);
+        try{
+            Object value = iast.getBody().accept(this, lexenv2);
+            i.send(this, "close",new Object[0]);
+            return value;
+        }
+        catch(Exception e){
+            i.send(this, "close",new Object[0]);
+            throw new EvaluationException(e.getMessage());
+        }
     }
     
 }
