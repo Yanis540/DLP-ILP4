073ebb469ac5fcd1d27351c4f8281795dc93ce3e
Yanis540 <yanistabellout4@gmail.com>
2023-12-21 01:47:44 +0100

    [partiel 2017] >>> switch working

--- Java/src/com/paracamplus/ilp4/partiel2017/interpreter/Interpreter.java
+++ Java/src/com/paracamplus/ilp4/partiel2017/interpreter/Interpreter.java
@@ -1,5 +1,6 @@
 package com.paracamplus.ilp4.partiel2017.interpreter;
 
+import com.paracamplus.ilp1.interfaces.IASTexpression;
 import com.paracamplus.ilp1.interpreter.interfaces.EvaluationException;
 import com.paracamplus.ilp1.interpreter.interfaces.IGlobalVariableEnvironment;
 import com.paracamplus.ilp1.interpreter.interfaces.ILexicalEnvironment;
@@ -17,8 +18,28 @@ implements IASTvisitor<Object, ILexicalEnvironment, EvaluationException> {
     }
 
     @Override
-    public Object visit(IASTswitch iast, ILexicalEnvironment data) throws EvaluationException {
-        return 1; 
+    public Object visit(IASTswitch iast, ILexicalEnvironment lexenv) throws EvaluationException {
+        Object argValue = iast.getArgument().accept(this, lexenv);
+        
+        IASTexpression[] cases = iast.getCases();
+        IASTexpression[] casesBody = iast.getCasesBody();
+        if(cases.length!=casesBody.length)
+            throw new EvaluationException("Should not occur");
+        int i = 0 ;
+        for(IASTexpression cas : cases ){
+            Object caseValue = cas.accept(this, lexenv);
+            if(!caseValue.equals(argValue) ){
+                i++;
+                continue ; 
+            }
+            IASTexpression casBody = casesBody[i]; 
+            return casBody.accept(this, lexenv);
+        }
+        IASTexpression otherwise = iast.getOtherwise();
+        if(otherwise == null)
+            return Boolean.FALSE; 
+         
+        return otherwise.accept(this, lexenv); 
     }
     
 }
