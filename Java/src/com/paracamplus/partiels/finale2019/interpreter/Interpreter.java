c4f6dd6ba15876c64a8a520e148ab7a1d159ca88
Yanis540 <yanistabellout4@gmail.com>
2023-12-27 21:48:48 +0100

    finished with the thing

--- Java/src/com/paracamplus/partiels/finale2019/interpreter/Interpreter.java
+++ Java/src/com/paracamplus/partiels/finale2019/interpreter/Interpreter.java
@@ -1,10 +1,14 @@
 package com.paracamplus.partiels.finale2019.interpreter;
 
+import java.util.Map;
+
 import com.paracamplus.ilp1.interpreter.interfaces.EvaluationException;
 import com.paracamplus.ilp1.interpreter.interfaces.IGlobalVariableEnvironment;
 import com.paracamplus.ilp1.interpreter.interfaces.ILexicalEnvironment;
 import com.paracamplus.ilp1.interpreter.interfaces.IOperatorEnvironment;
+import com.paracamplus.ilp4.interpreter.ILPInstance;
 import com.paracamplus.ilp4.interpreter.interfaces.IClassEnvironment;
+import com.paracamplus.ilp4.interpreter.interfaces.IMethod;
 import com.paracamplus.partiels.finale2019.interfaces.IASTautoClose;
 import com.paracamplus.partiels.finale2019.interfaces.IASTvisitor;
 
@@ -17,8 +21,28 @@ implements IASTvisitor<Object, ILexicalEnvironment, EvaluationException> {
     }
 
     @Override
-    public Object visit(IASTautoClose iast, ILexicalEnvironment data) throws EvaluationException {
-        return 1; 
+    public Object visit(IASTautoClose iast, ILexicalEnvironment lexenv) throws EvaluationException {
+        Object v =  iast.getValue().accept(this, lexenv);
+        if(!(v instanceof ILPInstance))
+            throw new EvaluationException("Expected Instance but got "+ v.getClass());
+        ILPInstance i = (ILPInstance) v; 
+        Map<String,IMethod> methods= i.classOf().getMethodDictionary();
+        if(! (methods.containsKey("close")))
+            throw new EvaluationException("close method doesn't exist ");
+        IMethod method = methods.get("close");
+        if(method.getArity()!=1)
+            throw new EvaluationException("Close method should not have paramters");
+        try{
+            ILexicalEnvironment lexenv2 = lexenv; 
+            lexenv2 = lexenv2.extend(iast.getVariable(), i);
+            v = iast.getBody().accept(this,lexenv2);
+            i.send(this,"close",new Object[0]);
+            return v; 
+        }
+        catch(Exception e){
+            i.send(this,"close",new Object[0]);
+            throw new EvaluationException(e.getMessage());
+        }
     }
     
 }
