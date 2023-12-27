028aed8d26699dd04226417429639b1661bcaabc
Yanis540 <yanistabellout4@gmail.com>
2023-12-27 16:52:54 +0100

    [Partiel 2022] : interprete working

--- Java/src/com/paracamplus/partiels/finale2022/interpreter/Interpreter.java
+++ Java/src/com/paracamplus/partiels/finale2022/interpreter/Interpreter.java
@@ -1,5 +1,10 @@
 package com.paracamplus.partiels.finale2022.interpreter;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.paracamplus.ilp1.interfaces.IASTexpression;
 import com.paracamplus.ilp1.interpreter.interfaces.EvaluationException;
 import com.paracamplus.ilp1.interpreter.interfaces.IGlobalVariableEnvironment;
 import com.paracamplus.ilp1.interpreter.interfaces.ILexicalEnvironment;
@@ -18,13 +23,40 @@ implements IASTvisitor<Object, ILexicalEnvironment, EvaluationException> {
     }
 
     @Override
-    public Object visit(IASTlist iast, ILexicalEnvironment data) throws EvaluationException {
-        return 1; 
+    public Object visit(IASTlist iast, ILexicalEnvironment lexenv) throws EvaluationException {
+        List<Object> obj =new ArrayList<>();
+        for(IASTexpression expr : iast.getExpressions() ){
+            Object value = expr.accept(this,lexenv);
+            obj.add(value);
+        }
+        return obj; 
     }
 
     @Override
-    public Object visit(IASTlistRange iast, ILexicalEnvironment data) throws EvaluationException {
-        return 2; 
+    public Object visit(IASTlistRange iast, ILexicalEnvironment lexenv) throws EvaluationException {
+        Object m = iast.getMax().accept(this,lexenv); 
+        if(!(m instanceof BigInteger))
+            throw new EvaluationException("Max should be an integer in range value");
+        int max = ((BigInteger)m).intValue();
+        if(max<0)
+            throw new EvaluationException("Max can't be negative");
+        List<Object> objects= new ArrayList<>();
+        ILexicalEnvironment lexenv2 = lexenv; 
+        lexenv2 = lexenv2.extend(iast.getVariable(), null);
+        for(int i=0; i<max ; i++){
+            lexenv2.update(iast.getVariable(), BigInteger.valueOf(i));
+            Object c = null;
+            if(iast.IsCondition()){
+                c = iast.getCondition().accept(this,lexenv2);
+            }
+            Boolean condition = c instanceof Boolean ? (( Boolean ) c) == true  : false; 
+            if((! iast.IsCondition()) || (condition)){
+                Object value = iast.getBody().accept(this,lexenv2); 
+                objects.add(value);
+            }
+            
+        }
+        return objects; 
     }
     
 }
--- Java/src/com/paracamplus/partiels/finale2022/interpreter/primitive/GetList.java
+++ Java/src/com/paracamplus/partiels/finale2022/interpreter/primitive/GetList.java
@@ -1,6 +1,7 @@
 package com.paracamplus.partiels.finale2022.interpreter.primitive;
 
 import java.io.IOException;
+import java.math.BigInteger;
 import java.util.List;
 
 import com.paracamplus.ilp1.interpreter.interfaces.EvaluationException;
@@ -20,7 +21,16 @@ public class GetList extends Primitive{
     @Override
 	public Object apply (Object l, Object i) throws EvaluationException {
         // cast l + i 
-        return 1; 
+        if(!(l instanceof List ))
+            throw new EvaluationException("Expected list but got" + l.getClass());
+        List<Object> list = (List) l; 
+        if(!(i instanceof BigInteger ))
+            throw new EvaluationException("Expected integer but got" + i.getClass());
+        int index = ((BigInteger) i ).intValue(); 
+        if (index <0 || index > list.size())
+            throw new EvaluationException("Index out of bound");
+        Object value = list.get(index); 
+        return value; 
     }
 
     
