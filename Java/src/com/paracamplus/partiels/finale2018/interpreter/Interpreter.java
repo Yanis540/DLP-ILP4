0c8b9fc9a5ce7b1ae6ad41d91860963d5c0f53b1
Yanis540 <yanistabellout4@gmail.com>
2023-12-30 01:14:01 +0100

    [partiel 2018] > working interprète

--- Java/src/com/paracamplus/partiels/finale2018/interpreter/Interpreter.java
+++ Java/src/com/paracamplus/partiels/finale2018/interpreter/Interpreter.java
@@ -1,10 +1,16 @@
 package com.paracamplus.partiels.finale2018.interpreter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.paracamplus.ilp1.interfaces.IASTexpression;
 import com.paracamplus.ilp1.interpreter.interfaces.EvaluationException;
 import com.paracamplus.ilp1.interpreter.interfaces.IGlobalVariableEnvironment;
 import com.paracamplus.ilp1.interpreter.interfaces.ILexicalEnvironment;
 import com.paracamplus.ilp1.interpreter.interfaces.IOperatorEnvironment;
 import com.paracamplus.ilp4.interpreter.interfaces.IClassEnvironment;
+import com.paracamplus.ilp4.interpreter.interfaces.IMethod;
+import com.paracamplus.ilp4.interpreter.interfaces.ISuperCallInformation;
 import com.paracamplus.partiels.finale2018.interfaces.IASTsuperWithArgs;
 import com.paracamplus.partiels.finale2018.interfaces.IASTvisitor;
 
@@ -17,7 +23,16 @@ implements IASTvisitor<Object, ILexicalEnvironment, EvaluationException> {
     }
     @Override 
     public Object visit(IASTsuperWithArgs iast, ILexicalEnvironment lexenv) throws EvaluationException {
-        return 1; 
+        ISuperCallInformation isci = 
+        		 ((com.paracamplus.ilp4.interpreter.interfaces.ISuperCallLexicalEnvironment) lexenv).getSuperCallInformation();
+        IMethod supermethod = isci.getSuperMethod();
+        List<Object> args = new ArrayList<>();
+        args.add(isci.getArguments()[0]); // adding self 
+        for(IASTexpression expr : iast.getExpressions()){
+            Object arg = expr.accept(this,lexenv);
+            args.add(arg);
+        }
+        return supermethod.apply(this, args.toArray());
     }
     
 }
