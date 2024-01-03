a6fb96fc4f83adc9d7c9604ea206e26f26a4dd1c
Yanis540 <yanistabellout4@gmail.com>
2024-01-03 18:12:26 +0100

    [partiel 2022] setup

---
+++ Java/src/com/paracamplus/partiels/finale2022_2/ast/ASTfactory.java
@@ -0,0 +1,6 @@
+package com.paracamplus.partiels.finale2022_2.ast;
+
+public class ASTfactory extends  com.paracamplus.ilp4.ast.ASTfactory 
+implements  com.paracamplus.partiels.finale2022_2.interfaces.IASTfactory {
+    
+}
---
+++ Java/src/com/paracamplus/partiels/finale2022_2/interfaces/IASTfactory.java
@@ -0,0 +1,5 @@
+package com.paracamplus.partiels.finale2022_2.interfaces;
+
+public interface IASTfactory extends com.paracamplus.ilp4.interfaces.IASTfactory{
+    
+}
